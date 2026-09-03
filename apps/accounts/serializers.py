babc01ad91f1ffd6c773b9bf5d45f1dde3560e28
Eli Arnaytska <eli_arnaytska@abv.bg>
2026-09-03 13:18:54 +0300

    Update serializers.py

---
+++ apps/accounts/serializers.py
@@ -0,0 +1,47 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+
+User = get_user_model()
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        write_only=True,
+        min_length=8,
+    )
+
+    password_confirm = serializers.CharField(
+        write_only=True,
+    )
+
+    class Meta:
+        model = User
+        fields = (
+            "username",
+            "email",
+            "password",
+            "password_confirm",
+        )
+
+    def validate(self, data):
+        if data["password"] != data["password_confirm"]:
+            raise serializers.ValidationError(
+                {
+                    "password_confirm": "Passwords do not match."
+                }
+            )
+
+        return data
+
+    def create(self, validated_data):
+        validated_data.pop("password_confirm")
+
+        password = validated_data.pop("password")
+
+        user = User(**validated_data)
+
+        user.set_password(password)
+        user.save()
+
+        return user

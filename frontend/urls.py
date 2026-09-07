64cb3254e6016937f60380463fce11a5db4fe688
Eli Arnaytska <eli_arnaytska@abv.bg>
2026-09-07 18:31:57 +0300

    Add files via upload

---
+++ frontend/urls.py
@@ -0,0 +1,35 @@
+from django.urls import path
+
+from .views import (
+    chat_view,
+    home_view,
+    login_view,
+    register_view,
+)
+
+
+urlpatterns = [
+    path(
+        "",
+        home_view,
+        name="home",
+    ),
+
+    path(
+        "login/",
+        login_view,
+        name="login",
+    ),
+
+    path(
+        "register/",
+        register_view,
+        name="register",
+    ),
+
+    path(
+        "chat/",
+        chat_view,
+        name="chat",
+    ),
+]

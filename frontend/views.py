295c3b85588b0f28e8b79ab655f3e5f7b1d46e12
Eli Arnaytska <eli_arnaytska@abv.bg>
2026-09-07 18:30:24 +0300

    Update views.py

--- frontend/views.py
+++ frontend/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
+from django.views.decorators.csrf import ensure_csrf_cookie
 
-# Create your views here.
+
+def home_view(request):
+    if request.user.is_authenticated:
+        return redirect("chat")
+
+    return redirect("login")
+
+
+@ensure_csrf_cookie
+def login_view(request):
+    if request.user.is_authenticated:
+        return redirect("chat")
+
+    return render(
+        request,
+        "frontend/login.html",
+    )
+
+
+@ensure_csrf_cookie
+def register_view(request):
+    if request.user.is_authenticated:
+        return redirect("chat")
+
+    return render(
+        request,
+        "frontend/register.html",
+    )
+
+
+@login_required(login_url="/login/")
+@ensure_csrf_cookie
+def chat_view(request):
+    return render(
+        request,
+        "frontend/chat.html",
+    )

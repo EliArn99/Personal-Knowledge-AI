6acc82b25f3c0ec0f445c55b4d024e1e7b7eaf74
Eli Arnaytska <eli_arnaytska@abv.bg>
2026-09-17 16:02:38 +0300

    Update views.py

--- apps/documents/views.py
+++ apps/documents/views.py
@@ -1,3 +1,65 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.parsers import (
+    FormParser,
+    MultiPartParser,
+)
+from rest_framework.permissions import (
+    IsAuthenticated,
+)
 
-# Create your views here.
+from .models import Document
+from .serializers import (
+    DocumentSerializer,
+)
+
+
+class DocumentListCreateAPIView(
+    generics.ListCreateAPIView
+):
+    serializer_class = (
+        DocumentSerializer
+    )
+
+    permission_classes = (
+        IsAuthenticated,
+    )
+
+    parser_classes = (
+        MultiPartParser,
+        FormParser,
+    )
+
+    def get_queryset(self):
+        return Document.objects.filter(
+            user=self.request.user
+        )
+
+    def perform_create(
+            self,
+            serializer,
+    ):
+        serializer.save(
+            user=self.request.user
+        )
+
+
+class DocumentDetailAPIView(
+    generics.RetrieveUpdateDestroyAPIView
+):
+    serializer_class = (
+        DocumentSerializer
+    )
+
+    permission_classes = (
+        IsAuthenticated,
+    )
+
+    parser_classes = (
+        MultiPartParser,
+        FormParser,
+    )
+
+    def get_queryset(self):
+        return Document.objects.filter(
+            user=self.request.user
+        )

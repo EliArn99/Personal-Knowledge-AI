8fa9f317d1c97f6d41bea5e4896cfd601c902d1b
Eli Arnaytska <eli_arnaytska@abv.bg>
2026-09-17 16:07:10 +0300

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,7 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-
 
 urlpatterns = [
     path(
@@ -30,4 +31,18 @@ urlpatterns = [
         "",
         include("apps.frontend.urls"),
     ),
+
+    path(
+        "api/documents/",
+        include(
+            "apps.documents.urls"
+        ),
+    ),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
